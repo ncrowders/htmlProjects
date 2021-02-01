811f5fbc6737b7d5b42dd325d0580829a0dacbe3
ncrowders <35735770+ncrowders@users.noreply.github.com>
2021-02-01 05:21:36 -0500

    third commit

--- indHome.js
+++ indHome.js
@@ -13,4 +13,35 @@
    elmnt.style.backgroundColor = color;
 }
 document.getElementById("defaultOpen").click();
-          
+
+// This for the log in Button
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+ 
+// Scroll To The Top Button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
